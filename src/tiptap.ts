dd85afdfad8adae5cf2b8ab8108dafd7e3594144
0xZensh <txr1883@gmail.com>
2023-09-29 23:52:21 +0800

    chore: update tiptap

--- src/tiptap.ts
+++ src/tiptap.ts
@@ -2,37 +2,24 @@
 import { nanoid } from 'nanoid'
 // import { generateJSON, generateHTML } from '@tiptap/html'
 import { generateJSON, generateHTML } from './html.js'
-import Color from '@tiptap/extension-color'
-import Bold from '@tiptap/extension-bold'
-import Document from '@tiptap/extension-document'
-import Blockquote from '@tiptap/extension-blockquote'
-import Code from '@tiptap/extension-code'
-import CodeBlock from '@tiptap/extension-code-block'
-// import FontFamily from '@tiptap/extension-font-family'
-import HardBreak from '@tiptap/extension-hard-break'
-import Heading from '@tiptap/extension-heading'
-import HorizontalRule from '@tiptap/extension-horizontal-rule'
-import Image from '@tiptap/extension-image'
-import Italic from '@tiptap/extension-italic'
-import Link from '@tiptap/extension-link'
-import ListItem from '@tiptap/extension-list-item'
-import Mention from '@tiptap/extension-mention'
-import OrderedList from '@tiptap/extension-ordered-list'
-import Paragraph from '@tiptap/extension-paragraph'
-import Subscript from '@tiptap/extension-subscript'
-import Superscript from '@tiptap/extension-superscript'
-import Table from '@tiptap/extension-table'
-import TableCell from '@tiptap/extension-table-cell'
-import TableHeader from '@tiptap/extension-table-header'
-import TableRow from '@tiptap/extension-table-row'
-import TaskItem from '@tiptap/extension-task-item'
-import TaskList from '@tiptap/extension-task-list'
-import Text from '@tiptap/extension-text'
-import TextAlign from '@tiptap/extension-text-align'
-import TextStyle from '@tiptap/extension-text-style'
-import Typography from '@tiptap/extension-typography'
-import Underline from '@tiptap/extension-underline'
-import Youtube from '@tiptap/extension-youtube'
+import { StarterKit } from '@tiptap/starter-kit'
+import { Color } from '@tiptap/extension-color'
+import { Image } from '@tiptap/extension-image'
+import { Link } from '@tiptap/extension-link'
+import { Mention } from '@tiptap/extension-mention'
+import { Subscript } from '@tiptap/extension-subscript'
+import { Superscript } from '@tiptap/extension-superscript'
+import { Table } from '@tiptap/extension-table'
+import { TableCell } from '@tiptap/extension-table-cell'
+import { TableHeader } from '@tiptap/extension-table-header'
+import { TableRow } from '@tiptap/extension-table-row'
+import { TaskItem } from '@tiptap/extension-task-item'
+import { TaskList } from '@tiptap/extension-task-list'
+import { TextAlign } from '@tiptap/extension-text-align'
+import { TextStyle } from '@tiptap/extension-text-style'
+import { Typography } from '@tiptap/extension-typography'
+import { Underline } from '@tiptap/extension-underline'
+import { Youtube } from '@tiptap/extension-youtube'
 
 import { Details } from '@tiptap-pro/extension-details'
 import { DetailsSummary } from '@tiptap-pro/extension-details-summary'
@@ -53,9 +40,9 @@ const uidTypes = [
   'paragraph',
   'tableHeader',
   'tableCell',
+  'taskItem',
 ]
 const tiptapExtensions = [
-  Document,
   Details.configure({
     persist: true,
   }),
@@ -66,31 +53,20 @@ const tiptapExtensions = [
     emojis: [...emojis],
   }),
   Color,
-  Bold,
-  Blockquote,
-  Code,
-  CodeBlock,
-  // FontFamily,
-  HardBreak,
-  Heading,
-  HorizontalRule,
   Image,
-  Italic,
   Link.configure({
-    openOnClick: false,
-    linkOnPaste: false,
+    protocols: [],
     autolink: false,
-    validate: isValidHref,
-    HTMLAttributes: {
-      rel: '',
-      target: '',
-    },
+    linkOnPaste: true,
+    openOnClick: false,
+    HTMLAttributes: { target: '_blank', rel: 'noopener noreferrer' },
+    validate: (href) => (href ? href.startsWith('https://') : false),
   }),
-  ListItem,
-  Mathematics,
+  Mathematics.configure({ katexOptions: { strict: false } }),
   Mention,
-  OrderedList,
-  Paragraph,
+  StarterKit.configure({
+    heading: { levels: [1, 2, 3, 4, 5, 6] },
+  }),
   Subscript,
   Superscript,
   Table,
@@ -101,8 +77,16 @@ const tiptapExtensions = [
     nested: true,
   }),
   TaskList,
-  Text,
-  TextAlign,
+  TextAlign.configure({
+    types: [
+      'heading',
+      'paragraph',
+      'codeBlock',
+      'blockquote',
+      'table',
+      'tableCell',
+    ],
+  }),
   TextStyle,
   Typography,
   Underline,
@@ -165,12 +149,8 @@ export class JSONDocumentAmender {
         if (mark.type === 'link' && mark.attrs != null) {
           delete mark.attrs.class
 
-          if (isSameOriginHref(mark.attrs.href)) {
-            delete mark.attrs.target
-          } else {
-            mark.attrs.rel = 'noopener noreferrer'
-            mark.attrs.target = '_blank'
-          }
+          mark.attrs.rel = 'noopener noreferrer'
+          mark.attrs.target = '_blank'
         }
       }
     }
@@ -216,24 +196,4 @@ export function findTitle(doc: Node, level: number): string {
     }
   }
   return ''
-}
-
-const LOCALHOST = 'https://localhost'
-function isSameOriginHref(href: string): boolean {
-  if (typeof href === 'string') {
-    try {
-      const url = new URL(href, LOCALHOST)
-      return url.origin === LOCALHOST
-    } catch (e) {}
-  }
-  return false
-}
-function isValidHref(href: string): boolean {
-  if (typeof href === 'string') {
-    try {
-      const url = new URL(href, LOCALHOST)
-      return url.protocol === 'https:' || url.protocol === 'mailto:'
-    } catch (e) {}
-  }
-  return false
 }
